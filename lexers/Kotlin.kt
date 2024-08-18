f7b7c43416dc41a9f96e33622cd097675beff706
zufuliu <zufuliu@gmail.com>
2024-08-18 19:33:27 +0800

    Add test file for Kotlin.

---
+++ lexers/Kotlin.kt
@@ -0,0 +1,40 @@
+#!ShebangLine
+
+// LineComment
+// TODO:
+//! TODO:
+/// TODO:
+
+/* DelimitedComment
+/*
+TODO:
+*/
+*/
+
+/** KDoc
+ * @param name
+ * TODO:
+ /*
+ */
+ */
+
+123_456 123_456U 123_456L
+123_456E+1 123_456E+1F
+0x123_456
+0b11_11
+
+'\t\u12345'
+
+`QuotedSymbol`
+
+"\t\u12345\$${1+1}$Identifier"
+
+"""multiline string
+\t\u12345\$${1+1}$Identifier
+"""""
+
+return@ Identifier		// RETURN_AT
+continue@ Identifier	// CONTINUE_AT
+break@ Identifier		// BREAK_AT
+this@Identifier			// THIS_AT
+super@Identifier		// SUPER_AT
