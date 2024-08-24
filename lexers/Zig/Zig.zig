6f1d032d5e101c0713f3150105a7b8c9c958193c
zufuliu <zufuliu@gmail.com>
2024-08-24 21:54:18 +0800

    Update Zig test.

--- lexers/Zig/Zig.zig
+++ lexers/Zig/Zig.zig
@@ -50,6 +50,14 @@ const binary_mask = 0b1_1111_1111;
 const permissions = 0o7_5_5;
 const big_address = 0xFF80_0000_0000_0000;
 
+const hex_floating_point = 0x103.70p-5;
+const another_hex_float = 0x103.70;
+const yet_another_hex_float = 0x103.70P-5;
+
+
+// You can also assign from a slice with comptime-known length to a vector using .*
+const vec2: @Vector(2, f32) = arr1[1..3].*;
+    
 const Point = struct {
     x: f32,
     y: f32,
@@ -70,4 +78,10 @@ const Payload = union {
     int: i64,
     float: f64,
     boolean: bool,
+};
+
+const FileOpenError = error {
+    AccessDenied,
+    OutOfMemory,
+    FileNotFound,
 };
