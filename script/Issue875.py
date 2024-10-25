3401885c76119a70fc9548a2612f23c45191aa62
zufuliu <zufuliu@gmail.com>
2024-10-25 18:08:54 +0800

    Add test script for https://github.com/zufuliu/notepad4/issues/875

---
+++ script/Issue875.py
@@ -0,0 +1,34 @@
+import os.path
+import random
+
+def GetFileExtList():
+	extList = ['empty']
+	path = 'assoc.log'
+	os.system(f'assoc > {path}')
+	if os.path.isfile(path):
+		with open(path, 'r', encoding='utf-8') as fd:
+			lines = fd.read().splitlines()
+			for line in lines:
+				index = line.find('=')
+				if index > 0:
+					extList.append(line[:index])
+	return extList
+
+def MakeTestDir(dirCount, fileCount):
+	extList = GetFileExtList()
+	print(f'dir: {dirCount}, file: {fileCount}, file ext: {len(extList)}')
+	root = 'test'
+	if not os.path.exists(root):
+		os.makedirs(root)
+	for i in range(dirCount):
+		path = rf'{root}\{i}'
+		if not os.path.exists(path):
+			os.makedirs(path)
+	for i in range(fileCount):
+		ext = random.choice(extList)
+		path = rf'{root}\{i}{ext}'
+		if not os.path.exists(path):
+			with open(path, 'wb') as fd:
+				pass
+
+# MakeTestDir(5000, 105_000)
